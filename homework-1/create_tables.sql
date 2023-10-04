ce5d8dce1fc3ae00c7ec467d49d48d1d7b0df21f
ganzolder <gnmsapr@gmail.com>
2023-10-05 01:29:44 +0700

    Done

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employee
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(200) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+
+CREATE TABLE customer
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+
+
+CREATE TABLE customer_order
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customer(customer_id) NOT NULL,
+	employee_id int REFERENCES employee(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+try:
+    conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='12345'
+    )
+    conn.autocommit = True
+
+except:
+    print('Не смог подключиться к БД')
+
+
+orders_data = r"C:\python\postgres-homeworks\homework-1\north_data\orders_data.csv"
+employees_data = r"C:\python\postgres-homeworks\homework-1\north_data\employees_data.csv"
+customers_data = r"C:\python\postgres-homeworks\homework-1\north_data\customers_data.csv"
+
+files_for_upload = {'employee': employees_data, 'customer': customers_data, 'customer_order': orders_data}
+
+try:
+    for key, value in files_for_upload.items():
+
+        with open(value, 'r') as db:
+            db_reader = csv.reader(db)
+
+            with conn.cursor() as cur:
+
+                for string in db_reader:
+
+                    if '_id' in string[0]:
+                        pass
+                    else:
+                        num_of_args = len(string) - 1
+                        sql_comm = f'INSERT INTO {key} VALUES ({"%s, "*num_of_args}%s)'
+                        cur.execute(sql_comm, string)
+finally:
+    conn.close()
