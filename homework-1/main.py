539b0bfc9049258d139321b519135bd883d03776
ganzolder <gnmsapr@gmail.com>
2023-10-07 00:44:20 +0700

    Done

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,42 +1,89 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 import csv
 import psycopg2
+import os
+
+start = input('Прежде чем программа начнет заполнение базы данных, убедитесь что имена файлов для заполняемых таблиц '
+      'строго соответсвуют названиям таблиц БД.\n\nНапример: если имя таблицы в БД table_name, то имя файла с данными должно быть '
+      'table_name.csv.\n\nНажите Enter для продолжения.')
+
+if start == '':
+
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            database='north',
+            user='postgres',
+            password='12345'
+        )
+        conn.autocommit = True
+
+    except:
+        print('Не смог подключиться к БД')
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+
+    # Извлекаем названия таблиц из результата запроса
+    table_names = cursor.fetchall()
+
+    # Задаем имена файлов, которые ищем
+    db_files = []
+
+    for t_name in table_names:
+        file_name = f'{t_name[0]}.csv'
+        db_files.append(file_name)
+
+    db_files.sort()
+
+    file_path = ''
+
+    # Получаем текущую директорию проекта
+    project_directory = os.path.abspath(os.path.dirname(__file__))
+
+    j = 0
+    # Идем по всем файлам в директории проекта и найдите нужный файл
+    for db_file_name in db_files:
+
+        for root, dirs, files in os.walk(project_directory):
+            if db_file_name in files:
+                file_path = os.path.join(root, db_file_name)
+                break
+
+    # Формируем путь для загрузки
+        data = file_path
+
+        for db_file_name in files:
+
+            if db_file_name in data:
+
+                files_for_upload = {db_file_name.split('.')[0]: data}
+                break
+
+        try:
+            rows = 0
+            for key, value in files_for_upload.items():
+
+                with open(value, 'r') as db:
+
+                    db_reader = csv.reader(db)
+
+                    with conn.cursor() as cur:
+
+                        for string in db_reader:
+
+                            if '_id' in string[0]:
+                                pass
+                            else:
+                                num_of_args = len(string) - 1
+                                sql_comm = f'INSERT INTO {key} VALUES ({"%s, " * num_of_args}%s)'
+                                cur.execute(sql_comm, string)
+                                rows += 1
+                        print(f'{db_file_name} загружен. Добавлено {rows} строк')
+        finally:
+            pass
+    else:
+        pass
 
 
-try:
-    conn = psycopg2.connect(
-        host='localhost',
-        database='north',
-        user='postgres',
-        password='12345'
-    )
-    conn.autocommit = True
 
-except:
-    print('Не смог подключиться к БД')
-
-
-orders_data = r"C:\python\postgres-homeworks\homework-1\north_data\orders_data.csv"
-employees_data = r"C:\python\postgres-homeworks\homework-1\north_data\employees_data.csv"
-customers_data = r"C:\python\postgres-homeworks\homework-1\north_data\customers_data.csv"
-
-files_for_upload = {'employee': employees_data, 'customer': customers_data, 'customer_order': orders_data}
-
-try:
-    for key, value in files_for_upload.items():
-
-        with open(value, 'r') as db:
-            db_reader = csv.reader(db)
-
-            with conn.cursor() as cur:
-
-                for string in db_reader:
-
-                    if '_id' in string[0]:
-                        pass
-                    else:
-                        num_of_args = len(string) - 1
-                        sql_comm = f'INSERT INTO {key} VALUES ({"%s, "*num_of_args}%s)'
-                        cur.execute(sql_comm, string)
-finally:
-    conn.close()
